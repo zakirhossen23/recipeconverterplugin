a3471aa4b45b73d59e4ddafd9b8e6f0a84c0eefa
Zakir Hossen <zakirhoss12345@gmail.com>
2021-10-30 05:37:15 +0600

    recipe

---
+++ recipeconverter.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Recipe Converter
+ * Version: 1.0.0
+ * Plugin URI: https://www.https://miseitmakeit.ca/
+ * Description: This is Recipe Converter plugin.
+ * Author: Suzanne Lejeune
+ * Author URI: https://www.https://miseitmakeit.ca/
+ * Text Domain: recipeconverter
+ *
+ * @package WordPress
+ * @author Suzanne Lejeune
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+add_filter( 'template_include', 'recipe_converter_page_template', 99 );
+function recipe_converter_page_template( $template ) {
+    if ( is_page( 'recipe-converter' )  ) {
+        $new_template = locate_template( array( 'portfolio-page-template.php' ) );
+		 return plugin_dir_path(__FILE__)."includes/custompage/customrecipe.php";
+  
+    }
+    return $template;
+}
