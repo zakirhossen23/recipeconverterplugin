b9681bbbe0dc0c1d294797d71c59351b6e8c7cce
Zakir Hossen <73053667+zakirhossen23@users.noreply.github.com>
2021-12-09 00:11:05 +0600

    fixed pdf pictures

--- includes/output/recipecard.php
+++ includes/output/recipecard.php
@@ -482,14 +482,14 @@ if (isset($_POST["html"])) {
             <tbody>
                 <tr>
                     <th style="width: 20%;">
-                        <img style="height: 33px;width: auto;" src="https://shopitmiseitmakeit.ca/wp-content\plugins\recipeconverter/assets/Shopit.png" />
+                        <img style="height: 33px;width: auto;" src="https://shopitmiseitmakeit.ca/wp-content/plugins/recipeconverter/assets/Shopit.png" />
                     </th>
                     <th style="width: 30%;">
-                        <img style="height: 33px;width: auto;" src="https://shopitmiseitmakeit.ca/wp-content\plugins\recipeconverter/assets/Miseit.png" />
+                        <img style="height: 33px;width: auto;" src="https://shopitmiseitmakeit.ca/wp-content/plugins/recipeconverter/assets/Miseit.png" />
 
                     </th>
                     <th style="width: 50%;">
-                        <img style="height: 33px;width: auto;" src="https://shopitmiseitmakeit.ca/wp-content\plugins\recipeconverter/assets/Makeit.png" />
+                        <img style="height: 33px;width: auto;" src="https://shopitmiseitmakeit.ca/wp-content/plugins/recipeconverter/assets/Makeit.png" />
 
 
                     </th>
